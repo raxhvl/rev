beb69b8ac2cfb1d9fd2cd52feada5ff75e411de7
rahul <raxhvl@users.noreply.github.com>
2025-01-26 07:49:51 +0000

    ✨ feat: custom payload

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,55 @@
-import fs from "fs";
 import { send } from "./lib/jwtClient";
 
 import request from "./request.json";
+import genesis from "./state/genesis.json";
 
 import "dotenv/config";
+import { buildStateTrieRoot } from "./state/trie";
+import { hexToBytes, bytesToHex } from "@ethereumjs/util";
+import { Wallet } from "@ethereumjs/wallet";
+import { LegacyTransaction } from "@ethereumjs/tx";
 
 const secret = process.env.JWT_SECRET || "";
 
 const main = async () => {
-  const response = await send(request, secret);
-  console.log(response);
+  //   const response = await send(request, secret);
+  //   console.log(response);
+
+  const account = new Wallet(hexToBytes(process.env.ACCOUNT_1_KEY));
+  console.log(account.getAddressString());
+
+  const tx = new LegacyTransaction({
+    to: "0x000000000000000000000000000000000000000a",
+    value: 1,
+    gasPrice: 1,
+    gasLimit: 21000,
+  });
+
+  const signedTx = tx.sign(account.getPrivateKey());
+
+  console.log(signedTx.toJSON());
+
+  //   console.log(signedTx);
+
+  console.log(await buildStateTrieRoot(genesis.alloc));
+
+  genesis.alloc = {
+    "0x0000000000000000000000000000000000000000": {
+      balance: "0x5208",
+    },
+    "0x000000000000000000000000000000000000000a": {
+      balance: "0x1",
+    },
+    "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266": {
+      balance: "0xef037",
+      nonce: "0x1",
+    },
+  };
+
+  console.log(
+    bytesToHex(await buildStateTrieRoot(genesis.alloc)) ==
+      "0xd077ec67768e27815486177abdd398fc21673526f18dd039f12ebdf3b8420e36"
+  );
 };
 
 main();
---
+++ src/state/trie.ts
@@ -0,0 +1,65 @@
+import { createMPT } from "@ethereumjs/mpt";
+import { RLP } from "@ethereumjs/rlp";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { hexToBytes } from "@ethereumjs/util";
+
+interface Account {
+  nonce: Buffer;
+  balance: Buffer;
+  code: Buffer;
+  storage: { [key: string]: Buffer };
+}
+
+interface WorldState {
+  [address: string]: Account;
+}
+
+/**
+ * Helper function to create a storage trie root from the Account's storage.
+ */
+async function buildStorageRoot(storage: {
+  [key: string]: Buffer;
+}): Promise<Buffer> {
+  const storageTrie = await createMPT({ useKeyHashing: true });
+  for (const slot in storage) {
+    const key = hexToBytes(slot);
+    const value = Buffer.from(RLP.encode(storage[slot]));
+    await storageTrie.put(key, value);
+  }
+  return Buffer.from(storageTrie.root());
+}
+
+/**
+ * Encodes an Account into Ethereum-like RLP:
+ * [ nonce, balance, storageRoot, codeHash ]
+ */
+async function encodeAccount(account: Account): Promise<Buffer> {
+  // Build the storage root
+  const storageRoot = await buildStorageRoot(account.storage);
+  const codeHash = keccak256(hexToBytes(account.code ?? "0x"));
+
+  // RLP encoding of scalar values MUST NOT have
+  // leading zero. (199)
+  // Positive integers must be represented in big-endian binary form with no
+  // leading zeroes (thus making the integer value zero equivalent to the empty byte array).
+  // See: https://github.com/ethereum/execution-specs/blob/4c7eaa840c421a1db2c01617532f31d08dc3dc6e/tests/test_rlp.py#L73
+
+  return Buffer.from(
+    RLP.encode([account.nonce, account.balance, storageRoot, codeHash])
+  );
+}
+
+/**
+ * Builds the state trie from the worldState object and returns the trie root.
+ */
+export async function buildStateTrieRoot(
+  worldState: WorldState
+): Promise<Buffer> {
+  const stateTrie = await createMPT({ useKeyHashing: true });
+  for (const address in worldState) {
+    const key = hexToBytes(address);
+    const encodedAccount = await encodeAccount(worldState[address]);
+    await stateTrie.put(key, encodedAccount);
+  }
+  return Buffer.from(stateTrie.root());
+}
