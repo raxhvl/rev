893ff91bd4c4fa040db26997d11888a1aa1c5a61
rahul <raxhvl@users.noreply.github.com>
2025-01-28 19:03:52 +0000

    🚧 wip: Block building

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,52 @@
 import "dotenv/config";
-import { buildStateTrieRoot } from "./vm/trie";
-import { hexToBytes, bytesToHex, intToHex } from "@ethereumjs/util";
-import { Wallet } from "@ethereumjs/wallet";
-import { LegacyTransaction } from "@ethereumjs/tx";
-import { formatSignedTx, t8n } from "./vm/t8n";
-import assert from "assert";
-import { send } from "./lib/jwtClient";
+import { createLegacyTx } from "@ethereumjs/tx";
+import { createBlock } from "@ethereumjs/block";
+import { Common, Mainnet } from "@ethereumjs/common";
+import { buildBlock, createVM } from "@ethereumjs/vm";
+import {
+  bytesToHex,
+  hexToBytes,
+  createAddressFromPrivateKey,
+  Account,
+} from "@ethereumjs/util";
 
 const secret = process.env.JWT_SECRET || "";
 
-const main = async () => {
-  if (!process.env.ACCOUNT_1_KEY) {
-    throw new Error(
-      "ACCOUNT_1_KEY is not defined in the environment variables"
-    );
-  }
-  const account = new Wallet(hexToBytes(process.env.ACCOUNT_1_KEY));
+const BENEFICIARY = "0x0000000000000000000000000000000000000000";
 
-  const tx = new LegacyTransaction({
-    to: "0x000000000000000000000000000000000000000a",
-    value: 1,
-    gasPrice: 1,
-    gasLimit: 21000,
+const main = async () => {
+  const common = new Common({ chain: Mainnet });
+  const vm = await createVM({ common });
+
+  const parentBlock = createBlock(
+    { header: { number: 1n } },
+    { skipConsensusFormatValidation: true }
+  );
+  const headerData = {
+    number: 2n,
+  };
+  const blockBuilder = await buildBlock(vm, {
+    parentBlock,
+    headerData,
+    blockOpts: {
+      calcDifficultyFromHeader: parentBlock.header,
+      freeze: false,
+      skipConsensusFormatValidation: true,
+      putBlockIntoBlockchain: false,
+    },
   });
 
-  const signedTx = tx.sign(account.getPrivateKey());
+  const pk = hexToBytes(process.env.ACCOUNT_1_KEY);
+  const address = createAddressFromPrivateKey(pk);
+  const account = new Account(0n, 0xfffffffffn);
+  await vm.stateManager.putAccount(address, account);
 
-  const preState = {
-    "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266": {
-      balance: "0xF4240",
-    },
-  };
+  const tx = createLegacyTx({ gasLimit: 0xffffff, gasPrice: 75n }).sign(pk);
+  await blockBuilder.addTransaction(tx);
 
-  const txs = [];
-  txs.push(signedTx.toJSON());
-
-  const { result, postState } = await t8n(preState, txs.map(formatSignedTx));
-
-  // Assumption: The state root is correct
-  assert(bytesToHex(await buildStateTrieRoot(postState)) == result.stateRoot);
-
-  const payload = {
-    jsonrpc: "2.0",
-    method: "engine_newPayloadV1",
-    params: [
-      {
-        parentHash:
-          "0x9cbea0de83b440f4462c8280a4b0b4590cdb452069757e2c510cb3456b6c98cc",
-        feeRecipient: "0x0000000000000000000000000000000000000000",
-        stateRoot: result.stateRoot,
-        receiptsRoot: result.receiptsRoot,
-        logsBloom: result.logsBloom,
-        prevRandao:
-          "0x9cbea0de83b440f4462c8280a4b0b4590cdb452069757e2c510cb3456b6c98cc",
-        blockNumber: "0x1",
-        gasLimit: "0x5d21dba00",
-        gasUsed: result.gasUsed,
-        timestamp: intToHex(Math.floor(Date.now() / 1000)),
-        extraData: "0x",
-        baseFeePerGas: result.currentBaseFee,
-        blockHash: result.receipts[0].blockHash,
-        transactions: [bytesToHex(signedTx.serialize())],
-      },
-    ],
-    id: 67,
-  };
-
-  const response = await send(payload, secret);
-
-  console.log(response);
+  // Add more transactions
+  const block = await blockBuilder.build();
+  console.log(`Built a block with hash ${bytesToHex(block.hash())}`);
 };
 
 main();
