d82601cf73ec94d2219c0605aae1214a2b1d0ffe
rahul <raxhvl@users.noreply.github.com>
2025-01-30 18:45:47 +0000

    🥢 nit: Handle errors

--- src/index.ts
+++ src/index.ts
@@ -12,64 +12,70 @@ import {
 import { send } from "./lib/jwtClient";
 
 const secret = process.env.JWT_SECRET || "";
-
+const accountKey = process.env.ACCOUNT_1_KEY || "";
 const BENEFICIARY = "0x0000000000000000000000000000000000000000";
 
 const main = async () => {
-  const common = new Common({ chain: Holesky });
-  const vm = await createVM({ common });
+  try {
+    const common = new Common({ chain: Holesky });
+    const vm = await createVM({ common });
 
-  const genesisBlock = createBlock({ header: { number: 0n } });
+    const genesisBlock = createBlock({ header: { number: 0n } });
 
-  const headerData: HeaderData = {
-    number: 1n,
-    timestamp: Math.floor(Date.now() / 1000),
-    coinbase: BENEFICIARY,
-    baseFeePerGas: 1n,
-  };
-  const blockBuilder = await buildBlock(vm, {
-    parentBlock: genesisBlock,
-    headerData,
-    blockOpts: {
-      calcDifficultyFromHeader: genesisBlock.header,
-      freeze: false,
-      skipConsensusFormatValidation: true,
-      putBlockIntoBlockchain: false,
-    },
-  });
+    const headerData: HeaderData = {
+      number: 1n,
+      timestamp: Math.floor(Date.now() / 1000),
+      coinbase: BENEFICIARY,
+      baseFeePerGas: 1n,
+    };
 
-  const pk = hexToBytes(process.env.ACCOUNT_1_KEY);
-  const address = createAddressFromPrivateKey(pk);
-  const account = new Account(0n, 0xfffffffffn);
-  await vm.stateManager.putAccount(address, account);
-
-  for (let i = 0; i < 10; i++) {
-    const tx = createLegacyTx(
-      {
-        nonce: i,
-        to: "0x000000000000000000000000000000000000000a",
-        value: 1,
-        gasPrice: 1,
-        gasLimit: 21000,
+    const blockBuilder = await buildBlock(vm, {
+      parentBlock: genesisBlock,
+      headerData,
+      blockOpts: {
+        calcDifficultyFromHeader: genesisBlock.header,
+        freeze: false,
+        skipConsensusFormatValidation: true,
+        putBlockIntoBlockchain: false,
       },
-      { common }
-    ).sign(pk);
+    });
 
-    await blockBuilder.addTransaction(tx);
+    const pk = hexToBytes(accountKey);
+    const address = createAddressFromPrivateKey(pk);
+    const account = new Account(0n, 0xfffffffffn);
+    await vm.stateManager.putAccount(address, account);
+
+    for (let i = 0; i < 10; i++) {
+      const tx = createLegacyTx(
+        {
+          nonce: i,
+          to: "0x000000000000000000000000000000000000000a",
+          value: 1,
+          gasPrice: 1,
+          gasLimit: 21000,
+        },
+        { common }
+      ).sign(pk);
+
+      await blockBuilder.addTransaction(tx);
+    }
+
+    const block = await blockBuilder.build();
+
+    const response = await send(
+      {
+        jsonrpc: "2.0",
+        method: "engine_newPayloadV1",
+        params: [block.toExecutionPayload()],
+        id: 67,
+      },
+      secret
+    );
+
+    console.log("Response:", response);
+  } catch (error) {
+    console.error("Error:", error);
   }
-
-  // Add more transactions
-  const block = await blockBuilder.build();
-
-  const response = await send(
-    {
-      jsonrpc: "2.0",
-      method: "engine_newPayloadV1",
-      params: [block.toExecutionPayload()],
-      id: 67,
-    },
-    secret
-  );
 };
 
 main();
